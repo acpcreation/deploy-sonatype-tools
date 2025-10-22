89607ad421482a1d62b74760a3b65621a172019e
Alexander Plattel <acpcreation@gmail.com>
2025-10-22 08:44:23 -0400

    Modify usage example and comment out variable parsing
    Updated example container name in usage comment and commented out variable parsing and echo statements for clarity.

--- sh/configure-container.sh
+++ sh/configure-container.sh
@@ -6,36 +6,35 @@ set -o pipefail
 # install awscli
 
 # USAGE: ./configure-container.sh <CONTAINER_NAME> <ECR_REPO_URI>
-# EXAMPLE: ./configure-container.sh sonatype/nexus3 470037505301.dkr.ecr.us-east-1.amazonaws.com/aplattel-container-registry
-
-# CONTAINER_NAME="sonatype/nexus3"
+# EXAMPLE: ./configure-container.sh sonatype/nexus-iq-server 470037505301.dkr.ecr.us-east-1.amazonaws.com/aplattel-container-registry
 # CONTAINER_NAME="sonatype/nexus-iq-server"
 # CONTAINER_NAME=$1
 # ECR_REPO_URI=$2
 
 # GET SG INPUT VARIABLES
-parse_variables() { 
-  [[ -f .env ]] && . .env
-  mountedWorkingDir="${LOCAL_IAC_SOURCE_CODE_DIR}" # TODO: should we provide ways to running ansible without anisble playbook, without VCS Config? Need use-cases!
-  mountedArtifactsDir="${LOCAL_ARTIFACTS_DIR}"
-  workflowStepInputParams=$(echo "${BASE64_WORKFLOW_STEP_INPUT_VARIABLES}" | base64 -d -i)
-  ansibleSSHPrivateKey="${ANSIBLE_SSH_PRIVATE_KEY}"
-  workflowIACInputVariables=$(echo "${BASE64_IAC_INPUT_VARIABLES}" | base64 -d -i)
-}
-parse_variables
-
-# playbookPath="$(echo "${workflowStepInputParams}" | jq -r '.playbookPath')"
+# echo "======================= BEGIN GET SG NO-CODE VARIABLES ======================="
+# parse_variables() { 
+#   [[ -f .env ]] && . .env
+#   mountedWorkingDir="${LOCAL_IAC_SOURCE_CODE_DIR}" # TODO: should we provide ways to running ansible without anisble playbook, without VCS Config? Need use-cases!
+#   mountedArtifactsDir="${LOCAL_ARTIFACTS_DIR}"
+#   workflowStepInputParams=$(echo "${BASE64_WORKFLOW_STEP_INPUT_VARIABLES}" | base64 -d -i)
+#   ansibleSSHPrivateKey="${ANSIBLE_SSH_PRIVATE_KEY}"
+#   workflowIACInputVariables=$(echo "${BASE64_IAC_INPUT_VARIABLES}" | base64 -d -i)
+# }
+# parse_variables
  
+# echo "Workflow IAC Input Variables: $workflowIACInputVariables"
+# CONTAINER_NAME="$(echo "${workflowIACInputVariables}" | jq -r '.CONTAINER_NAME_INPUT')"
+# ECR_REPO_URI="$(echo "${workflowIACInputVariables}" | jq -r '.ECR_REPO_URI_INPUT')"  
 
-echo "Input Params: $workflowIACInputVariables"
-echo "========================================================="
+# # LOCAL VARIABLES
+# echo "CONTAINER NAME: $CONTAINER_NAME"
+# echo "ECR REPO: $ECR_REPO_URI"
 
-CONTAINER_NAME="$(echo "${workflowIACInputVariables}" | jq -r '.CONTAINER_NAME_INPUT')"
-ECR_REPO_URI="$(echo "${workflowIACInputVariables}" | jq -r '.ECR_REPO_URI_INPUT')"  
+# echo "======================= END GET SG NO-CODE VARIABLES ======================="
 
-echo "CONTAINER NAME: $CONTAINER_NAME"
-echo "ECR REPO: $ECR_REPO_URI"
-echo "========================================================="
+echo "SG INPUT:  $CONTAINER_NAME_INPUT"
+
 
 
 CONTAINER_TAG="latest"
